55b2c7b617d6273fb197cfab72bf11481cb3687b
winEX <39057990+winEX13@users.noreply.github.com>
2025-01-14 10:34:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pyppeteer.errors import TimeoutError
 from fastapi import FastAPI, HTTPException
 import json
 from typing import Optional
+import re
+import datetime
 
 app = FastAPI()
 
@@ -60,47 +62,32 @@ async def scrape_data(url: str):
             "//div[@class='masstransit-timetable-view__transports']/li[@class='masstransit-vehicle-snippet-view _clickable _type_bus']/div[@class='masstransit-vehicle-snippet-view__row']/div[contains(@class, 'masstransit-vehicle-snippet-view__info') or contains(@class, 'masstransit-vehicle-snippet-view__prognoses')]//*[contains(@class, 'masstransit-vehicle-snippet-view__main-text') or contains(@class, 'masstransit-timetable-view__time')]",
             'textContent'
         )
-        return {
-            f"[{i}] {k}": v.replace('\xa0', ' ')
-            for i, (k, v) in enumerate(zip(schedule[::2], schedule[1::2]), 1)
-        }
+        schedule = [(k, v.replace('\xa0', ' '))
+            for k, v in zip(schedule[::2], schedule[1::2]) if re.fullmatch(r'\d{2}:\d{2}', v)
+        ]
+        def suffix(number, singular, few, many):
+            number = abs(number) % 100
+            if 11 <= number <= 19: return many
+            last_digit = number % 10
+            if last_digit == 1: return singular
+            elif 2 <= last_digit <= 4: return few
+            else: return many
+        def left(duration):
+            days, seconds = duration.days, duration.seconds
+            hours = days * 24 + seconds // 3600
+            minutes = (seconds % 3600) // 60
+            seconds = seconds % 60
+            return duration.total_seconds(), f"Осталось {minutes} {suffix(minutes, 'минута', 'минуты', 'минут')} и {seconds} {suffix(seconds, 'секунда', 'секунды', 'секунд')}"
+        now = datetime.datetime.now()
+        return [{'name': k, 'time': v} | dict(zip(('total', 'left'), left(datetime.datetime.strptime(v, '%H:%M').replace(year=now.year, month=now.month, day=now.day) - now))) for k, v in schedule]
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     finally:
         await page.close()
 
 @app.get('/')
-async def scrape_endpoint(url: Optional[str] = None):
+async def scrape_endpoint():
     url = 'https://yandex.ru/maps/213/moscow/stops/stop__9645057/?ll=37.742975%2C55.651185&tab=timetable&z=10'
     data = await scrape_data(url)
     await stop_browser()
     return data
-
-# async def get(page):
-#     await page.reload()
-#     return await getElement(page, "//div[@class='masstransit-timetable-view__transports']/li[@class='masstransit-vehicle-snippet-view _clickable _type_bus']/div[@class='masstransit-vehicle-snippet-view__row']/div[contains(@class, 'masstransit-vehicle-snippet-view__info') or contains(@class, 'masstransit-vehicle-snippet-view__prognoses')]//*[contains(@class, 'masstransit-vehicle-snippet-view__main-text') or contains(@class, 'masstransit-timetable-view__time')]", 'textContent')
-
-# async def main(url):
-#     browser = await connect()
-    
-#     page = await browser.newPage()
-#     await stealth(page)
-    
-#     response = await page.goto(url, timeout=0)
-#     await page.waitFor(1000)
-    
-#     schedule = await get(page)
-    
-#     # await page.reload()
-    
-#     # schedule = await getElement(page, "//div[@class='masstransit-timetable-view__transports']/li[@class='masstransit-vehicle-snippet-view _clickable _type_bus']/div[@class='masstransit-vehicle-snippet-view__row']/div[contains(@class, 'masstransit-vehicle-snippet-view__info') or contains(@class, 'masstransit-vehicle-snippet-view__prognoses')]//*[contains(@class, 'masstransit-vehicle-snippet-view__main-text') or contains(@class, 'masstransit-timetable-view__time')]", 'textContent')
-    
-#     await browser.close()
-    
-#     return json.dumps({f'[{i}] {k}': v.replace('\xa0', ' ') for i, (k, v) in enumerate(zip(schedule[::2], schedule[1::2]), 1)})
-    
-# if __name__ == '__main__':
-#     url = 'https://yandex.ru/maps/213/moscow/stops/stop__9645057/?ll=37.742975%2C55.651185&tab=timetable&z=10'
-#     router.add_api_route('/', endpoint=get)
-#     app.include_router(router)
-#     asyncio.run(main(url))
